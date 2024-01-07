17653e89d5e6bc37a1a39ba1ebdea060961d72ed
Fiorainnnn <11046029@ntub.edu.tw>
2024-01-08 07:24:22 +0800

    新增 - 永豐金api測試、yaml配置檔

--- backend/tests.py
+++ backend/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+import shioaji as sj
+import yaml
 
-# Create your tests here.
+with open('config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+
+class ShioajiTestCase(TestCase):
+    def test_virtual_account_login(self):
+        api = sj.Shioaji(simulation=True)  # 模擬模式
+        api.login(
+            api_key=config["shioaji"]["api_key"],
+            secret_key=config["shioaji"]["secret_key"],
+            contracts_cb=lambda security_type: print(
+                f"{repr(security_type)} fetch done."
+            ),  # 獲取商品檔Callback
+        )
